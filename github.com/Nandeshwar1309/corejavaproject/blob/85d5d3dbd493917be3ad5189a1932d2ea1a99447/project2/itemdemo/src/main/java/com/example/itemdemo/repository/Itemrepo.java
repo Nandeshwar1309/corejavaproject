9e9441f7e805a0504a8035d5b2cc4dacd39bed2f
Nandeshwar <nandeshwarveerabhrama2003@gmail.com>
2024-10-10 18:43:26 +0530

    Delete github.com/Nandeshwar1309/corejavaproject/blob/85d5d3dbd493917be3ad5189a1932d2ea1a99447/project2/itemdemo directory

--- github.com/Nandeshwar1309/corejavaproject/blob/85d5d3dbd493917be3ad5189a1932d2ea1a99447/project2/itemdemo/src/main/java/com/example/itemdemo/repository/Itemrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.itemdemo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.itemdemo.entity.Item;
-
-public interface Itemrepo extends CrudRepository<Item, Integer> {
-
-}
